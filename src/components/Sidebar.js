9cdccd149fb6515f2fe22e48b7d80a5e214c5155
Elian Valdez <elian.valdez09@gmail.com>
2024-02-26 22:53:47 -0300

    DataTable Concluded | Media queries corrections

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,7 +1,7 @@
 export default function Sidebar() {
 
   return (
-    <div className="w-full sm:h-20 md:w-28 md:h-screen p-3 flex md:flex-col md:justify-center sm:align-center sm:flex-row">
+    <div className="w-full p-3 flex sm:h-20 sm:align-center sm:flex-row lg:w-28 lg:h-screen  md:flex-col md:justify-center ">
       a
     </div>
   )
